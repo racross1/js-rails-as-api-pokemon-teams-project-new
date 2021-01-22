ca7f1197ee2f0e8ef8ae2be1c3db31e01043c3fb
racross1 <racross1@gmail.com>
2021-01-22 11:44:07 -0800

    done

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -8,21 +8,4 @@ class TrainersController < ApplicationController
         }, :except => [:created_at, :updated_at])
     end 
 
-
-    # def index
-    #     sightings = Sighting.all
-    #     render json: sightings.to_json(:include => {
-    #       :bird => {:only => [:name, :species]},
-    #       :location => {:only => [:latitude, :longitude]}
-    #     }, :except => [:updated_at])
-    #   end
-
-    # def show
-    #     sighting = Sighting.find_by(id: params[:id])
-    #     render json: sighting.to_json(:include => {
-    #       :bird => {:only => [:name, :species]},
-    #       :location => {:only => [:latitude, :longitude]}
-    #     }, :except => [:updated_at])
-    #   end 
-
 end
